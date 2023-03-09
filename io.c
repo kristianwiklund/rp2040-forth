a5552d31f471a107dbb091c4fec91f959ec9803a
Kristian Wiklund <githubkristian@snabela.nl>
2023-03-09 18:38:52 +0000

    x

---
+++ io.c
@@ -0,0 +1,22 @@
+#ifdef NUCLEO_F103
+
+// set up the UART
+void stdio_init_all() {
+}
+
+// read with timeout
+char getchar_timeout_us()  {
+}
+
+char getchar() {
+}
+
+void _putchar(char c) {
+}
+
+void putchar(char c) {
+  _putchar(c);
+}
+
+
+#endif
